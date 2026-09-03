71fb445c021a2cb6b9ca896ba952755449e2975a
RinOniz <khanhthien040805@gmail.com>
2026-09-03 14:53:13 +0700

    create Board structure, GameController and create Tile prefab; instantiate grid layout

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField] private GameObject board;
+    [SerializeField] private GameObject tilePrefab;
+
+    private const int totalRows = 10;
+    private const int totalCols = 18;
+
+    public static int totalTiles = 8 * 16;
+
+    private float startX = -9.2f;
+    private float startY = 4.5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        CreateBoard();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void CreateBoard()
+    {
+        for (int row = 0; row < totalRows; row++)
+        {
+            for (int col = 0; col < totalCols; col++)
+            {
+                if (row == 0 || row == totalRows - 1 || col == 0 || col == totalCols - 1)
+                {
+                    continue;
+                }
+                else
+                {
+                    Vector3 position = new Vector3(startX + col, startY - row, 0.0f);
+                    GameObject newTileObj = Instantiate(tilePrefab, position, Quaternion.identity) as GameObject;
+
+                    newTileObj.name = "Tile[" + row + ", " + col + "]";
+                    newTileObj.transform.localScale = new Vector3(0.76f, 0.76f, 1f);
+                    newTileObj.transform.SetParent(board.transform);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private GameObject itemBackground;
+
+    public int row, col, id;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
