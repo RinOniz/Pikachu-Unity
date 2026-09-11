d65f065a8508b22d8dca76de554402f9cfb65232
RinOniz <khanhthien040805@gmail.com>
2026-09-11 16:49:37 +0700

    implement straight line check algorithm for tiles

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -118,4 +118,103 @@ public class Board : MonoBehaviour
             }
         }
     }
+
+    public bool CheckLine(Position startPos, Position endPos, bool drawPath)
+    {
+        if (startPos.row == endPos.row && CheckLineCol(startPos.row, startPos.col, endPos.col))
+        {
+            if (drawPath)
+            {
+                return true;
+            }
+        }
+
+        if (startPos.col == endPos.col && CheckLineRow(startPos.col, startPos.row, endPos.row))
+        {
+            if (drawPath)
+            {
+                return true;
+            }
+        }
+
+        if (idGrid[startPos.row, endPos.col] == -1 &&
+            CheckLineCol(startPos.row, startPos.col, endPos.col) &&
+            CheckLineRow(endPos.col, startPos.row, endPos.row))
+        {
+            if (drawPath)
+            {
+                return true;
+            }
+        }
+
+        if (idGrid[endPos.row, startPos.col] == -1 &&
+            CheckLineRow(startPos.col, startPos.row, endPos.row) &&
+            CheckLineCol(endPos.row, startPos.col, endPos.col))
+        {
+            if (drawPath) 
+                return true;
+        }
+
+        for (int r = 0; r < totalRows; r++)
+        {
+            if (r != startPos.row && r != endPos.row && idGrid[r, startPos.col] == -1 && idGrid[r, endPos.col] == -1)
+            {
+                if (CheckLineRow(startPos.col, startPos.row, r) &&
+                    CheckLineRow(endPos.col, endPos.row, r) &&
+                    CheckLineCol(r, startPos.col, endPos.col))
+                {
+                    if (drawPath)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        for (int c = 0; c < totalCols; c++)
+        {
+            if (c != startPos.col && c != endPos.col && idGrid[startPos.row, c] == -1 && idGrid[endPos.row, c] == -1)
+            {
+                if (CheckLineCol(startPos.row, startPos.col, c) &&
+                    CheckLineCol(endPos.row, endPos.col, c) &&
+                    CheckLineRow(c, startPos.row, endPos.row))
+                {
+                    if (drawPath) 
+                        return true;
+                }
+            }
+        }
+
+        return false;
+    }
+         
+    private bool CheckLineRow(int col, int row1, int row2)
+    {
+        int min = Mathf.Min(row1, row2);
+        int max = Mathf.Max(row1, row2);
+
+        for (int r = min + 1; r < max; r++)
+        {
+            if (idGrid[r, col] != -1)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private bool CheckLineCol(int row, int col1, int col2)
+    {
+        int min = Mathf.Min(col1, col2);
+        int max = Mathf.Max(col1, col2);
+
+        for (int c = min + 1; c < max; c++)
+        {
+            if (idGrid[row, c] != -1)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 }
